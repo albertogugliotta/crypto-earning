dcafab78b3f0983bfb7eaecf253fafe560dd06a8
Ironal <alberto.gugliotta@gmail.com>
2017-12-03 14:43:20 -0200

    Improved sample transactions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const mcdBrl = "https://www.mercadobitcoin.net/api/BTC/ticker";
 const cexEur = "https://cex.io/api/last_price/BTC/EUR";
 const cexGbp = "https://cex.io/api/last_price/BTC/GBP";
 
-/* Add your transactions here below - Modify amountBTC & amountPaid */
+/* Sample transactions below. Add your transactions instead or just modify amountBTC & amountPaid */
 transactions.push({ currency : 'BRL', amountBTC : 0.098583, amountPaid : 2000,  url: mcdBrl });
 transactions.push({ currency : 'EUR', amountBTC : 0.075435, amountPaid : 500,   url: cexEur });
 transactions.push({ currency : 'GBP', amountBTC : 0.085432, amountPaid : 500,   url: cexGbp });
